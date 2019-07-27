4fdd55db76dd75ebaa408a35c8224f4a11b995e5
shtrih <shtrih@hitagi.ru>
2019-07-27 13:42:26 +0400

    Fix: firefox compatibility. More simple regexp works for now

--- tweetdeck-image-to-discord.user.js
+++ tweetdeck-image-to-discord.user.js
@@ -51,7 +51,7 @@
             isDiscord = $(e.target).hasClass('dscrd')
             , tweetHeader = $imageContainer.parents('.js-stream-item-content').find('.js-tweet-header')
             , tweetAuthorLogin = $('.username', tweetHeader).text()
-            , imageUri = $imageContainer.attr('style').replace('background-image:url(', '')/*.replace('?format=jpg', '')*/.replace(/(&name=|(?<!ttps):)[^:)]+[)]$/, '')
+            , imageUri = $imageContainer.attr('style').replace('background-image:url(', '')/*.replace('?format=jpg', '')*/.replace(/&name=[^"]+"[)];$/, '')
             // , tweetAuthorAvatar = $('img.tweet-avatar', tweetHeader).attr('src')
         ;
 
