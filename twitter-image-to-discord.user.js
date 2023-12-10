15860be3a0a9ef6b8876ecbe23f091dac160ac55
Dmitry Morozov <s@hitagi.ru>
2023-12-10 14:28:21 +0400

    Feat: added message button

--- twitter-image-to-discord.user.js
+++ twitter-image-to-discord.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         twitter-image-to-discord.user.js
 // @namespace    https://github.com/shtrih
-// @version      2.0.3
+// @version      2.1
 // @description  Repost Image to Discord via Webhook in one click!
 // @author       shtrih
 // @match        https://twitter.com/*
@@ -18,6 +18,7 @@
 // @homepage     https://github.com/shtrih/twitter-repost-image-to-discord
 // @supportURL   https://github.com/shtrih/twitter-repost-image-to-discord/issues
 // @downloadURL  https://github.com/shtrih/twitter-repost-image-to-discord/raw/master/twitter-image-to-discord.user.js
+// @updateURL    https://github.com/shtrih/twitter-repost-image-to-discord/raw/master/twitter-image-to-discord.user.js
 // @icon         https://tweetdeck.twitter.com/favicon.ico
 // ==/UserScript==
 
@@ -65,11 +66,13 @@ function run () {
         }
         .share-42 {
             position: absolute; 
-            background: rgba(255, 255, 255, 0.9); 
+            background: rgba(250, 250, 250, 0.9);
             font-size: 14px; 
-            padding: 0 4px; 
+            padding: 0 2px;
+            margin: 0 2px;
             z-index: 2;
             color: #555;
+            line-height: 18px;
         }
         .share-42:hover {
             text-decoration: underline;
@@ -89,7 +92,7 @@ function run () {
     const settingsIcon = '<svg viewBox="0 0 24 24" class="settings"><g><path d="M12 8.21c-2.09 0-3.79 1.7-3.79 3.79s1.7 3.79 3.79 3.79 3.79-1.7 3.79-3.79-1.7-3.79-3.79-3.79zm0 6.08c-1.262 0-2.29-1.026-2.29-2.29S10.74 9.71 12 9.71s2.29 1.026 2.29 2.29-1.028 2.29-2.29 2.29z"></path><path d="M12.36 22.375h-.722c-1.183 0-2.154-.888-2.262-2.064l-.014-.147c-.025-.287-.207-.533-.472-.644-.286-.12-.582-.065-.798.115l-.116.097c-.868.725-2.253.663-3.06-.14l-.51-.51c-.836-.84-.896-2.154-.14-3.06l.098-.118c.186-.222.23-.523.122-.787-.11-.272-.358-.454-.646-.48l-.15-.014c-1.18-.107-2.067-1.08-2.067-2.262v-.722c0-1.183.888-2.154 2.064-2.262l.156-.014c.285-.025.53-.207.642-.473.11-.27.065-.573-.12-.795l-.094-.116c-.757-.908-.698-2.223.137-3.06l.512-.512c.804-.804 2.188-.865 3.06-.14l.116.098c.218.184.528.23.79.122.27-.112.452-.358.477-.643l.014-.153c.107-1.18 1.08-2.066 2.262-2.066h.722c1.183 0 2.154.888 2.262 2.064l.014.156c.025.285.206.53.472.64.277.117.58.062.794-.117l.12-.102c.867-.723 2.254-.662 3.06.14l.51.512c.836.838.896 2.153.14 3.06l-.1.118c-.188.22-.234.522-.123.788.112.27.36.45.646.478l.152.014c1.18.107 2.067 1.08 2.067 2.262v.723c0 1.183-.888 2.154-2.064 2.262l-.155.014c-.284.024-.53.205-.64.47-.113.272-.067.574.117.795l.1.12c.756.905.696 2.22-.14 3.06l-.51.51c-.807.804-2.19.864-3.06.14l-.115-.096c-.217-.183-.53-.23-.79-.122-.273.114-.455.36-.48.646l-.014.15c-.107 1.173-1.08 2.06-2.262 2.06zm-3.773-4.42c.3 0 .593.06.87.175.79.328 1.324 1.054 1.4 1.896l.014.147c.037.4.367.7.77.7h.722c.4 0 .73-.3.768-.7l.014-.148c.076-.842.61-1.567 1.392-1.892.793-.33 1.696-.182 2.333.35l.113.094c.178.148.366.18.493.18.206 0 .4-.08.546-.227l.51-.51c.284-.284.305-.73.048-1.038l-.1-.12c-.542-.65-.677-1.54-.352-2.323.326-.79 1.052-1.32 1.894-1.397l.155-.014c.397-.037.7-.367.7-.77v-.722c0-.4-.303-.73-.702-.768l-.152-.014c-.846-.078-1.57-.61-1.895-1.393-.326-.788-.19-1.678.353-2.327l.1-.118c.257-.31.236-.756-.048-1.04l-.51-.51c-.146-.147-.34-.227-.546-.227-.127 0-.315.032-.492.18l-.12.1c-.634.528-1.55.67-2.322.354-.788-.327-1.32-1.052-1.397-1.896l-.014-.155c-.035-.397-.365-.7-.767-.7h-.723c-.4 0-.73.303-.768.702l-.014.152c-.076.843-.608 1.568-1.39 1.893-.787.326-1.693.183-2.33-.35l-.118-.096c-.18-.15-.368-.18-.495-.18-.206 0-.4.08-.546.226l-.512.51c-.282.284-.303.73-.046 1.038l.1.118c.54.653.677 1.544.352 2.325-.327.788-1.052 1.32-1.895 1.397l-.156.014c-.397.037-.7.367-.7.77v.722c0 .4.303.73.702.768l.15.014c.848.078 1.573.612 1.897 1.396.325.786.19 1.675-.353 2.325l-.096.115c-.26.31-.238.756.046 1.04l.51.51c.146.147.34.227.546.227.127 0 .315-.03.492-.18l.116-.096c.406-.336.923-.524 1.453-.524z"></path></g></svg>',
         getReposterForm = (reposterNickname, isAuthorInText) => `<div class="reposter">
             <p><label>Your nickname: 
-                <input type="text" placeholder="" name="nickname" value="${reposterNickname}" /></label></p>
+                <input type="text" placeholder="Optional" name="nickname" value="${reposterNickname}" /></label></p>
             <p>
                 Image's author placement:
                 <label><input type="radio" name="authorInText" value="0" ${isAuthorInText ? '' : 'checked'} />in nickname</label>
@@ -101,7 +104,8 @@ function run () {
             <p><label>Title: <input type="text" placeholder="Post to Discord" value="${title}" /></label></p>
             <p><label>Webhook URL: <input type="url" placeholder="https://discord.com/api/webhooks/00000000000000000000/XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX" pattern="^https?:[/]{2}(discord(app)?[.]com)[/].+" value="${hook}" /></label></p>
         </div>`,
-        spoilerTitle = '■',
+        spoilerTitle = '🕶️',
+        textMessageTitle = '📝',
         getShareLinks = (title, hookIndex) => `<div class="btn-link share-42" data-hook-index="${hookIndex}">${title}</div>`,
         hook = function (title = '', uri = '') {
             return {title, uri}
@@ -126,7 +130,7 @@ function run () {
             }
 
             dialogAddButton.before(
-                getReposterForm(config.reposterNickname || '',  config.authorInText || 0)
+                getReposterForm(config.reposterNickname || '', config.authorInText || 0)
             );
             dialogAddButton.before(
                 config.hooks.map((hook) => getHooksForm(hook.title, hook.uri)).join('')
@@ -173,8 +177,8 @@ function run () {
                 const tweetHeader = $imageContainer.parents('.js-stream-item-content').find('.js-tweet-header');
                 tweetAuthorLogin = $('.username', tweetHeader).text();
                 imageUri = $imageContainer.attr('style')
-                    // background-image:url(https://pbs.twimg.com/media/EAuEVnVUYAId48z.jpg?format=jpg&name=small) →
-                    // → https://pbs.twimg.com/media/EAuEVnVUYAId48z.jpg:orig
+                // background-image:url(https://pbs.twimg.com/media/EAuEVnVUYAId48z.jpg?format=jpg&name=small) →
+                // → https://pbs.twimg.com/media/EAuEVnVUYAId48z.jpg:orig
                     .replace('background-image:url(', '')
                     .replace(/[?]format=[a-z]+/, '')
                     .replace(/&name=[^)]+[)]$/, ':orig')
@@ -224,6 +228,9 @@ function run () {
             data.content = imageUri;
             if (shareLink.text() === spoilerTitle) {
                 data.content = `|| ${data.content} ||`;
+            } else if (shareLink.text() === textMessageTitle) {
+                let message = prompt("Enter your message");
+                data.content = `${message}\n${data.content}`;
             }
 
             if (config.reposterNickname) {
@@ -264,9 +271,15 @@ function run () {
                 link.on('click', shareClickHandler);
                 link.append(
                     $(getShareLinks(spoilerTitle, i))
-                        .attr('title', 'Add spoiler')
+                        .attr('title', 'With spoiler')
                         .attr('style', 'display: inline-block')
                         .on('click', shareClickHandler)
+                        .append(
+                            $(getShareLinks(textMessageTitle, i))
+                                .attr('title', 'With text message')
+                                .attr('style', 'display: inline-block')
+                                .on('click', shareClickHandler)
+                        )
                 );
 
                 shareButtons.append(link);
